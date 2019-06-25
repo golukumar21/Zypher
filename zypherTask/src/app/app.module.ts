ed270a86254e6645e8476bc3fede794a6c070637
Kumar Abhishek <golukumar21@gmail.com>
2019-06-25 14:41:49 +0530

    final commit

--- zypherTask/src/app/app.module.ts
+++ zypherTask/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { GeneralService } from '../general.service';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgxLoadingModule } from 'ngx-loading';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,9 +19,14 @@ import { ListDetailsComponent } from './list-details/list-details.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxLoadingModule.forRoot({}),
+    NgxSpinnerModule
   ],
   providers: [GeneralService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    NgxSpinnerModule
+  ]
 })
 export class AppModule { }
--- zypherTask/src/app/list-details/list-details.component.ts
+++ zypherTask/src/app/list-details/list-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GeneralService } from '../../general.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-list-details',
@@ -9,14 +10,20 @@ import { GeneralService } from '../../general.service';
 })
 export class ListDetailsComponent implements OnInit {
 
-  constructor(private gs: GeneralService, private route: ActivatedRoute) { }
+  constructor(private gs: GeneralService, private route: ActivatedRoute, private spinner: NgxSpinnerService) { }
   id: number;
   private sub: any;
   itemDetails: any;
   gId: any;
   items: any;
+  public loading = false;
+  noDetails: boolean = false;
 
   ngOnInit() {
+    // this.spinner.show();
+
+    this.loading = true;
+
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id']; 
     });
@@ -25,11 +32,17 @@ export class ListDetailsComponent implements OnInit {
   }
 
   showListDetails(arg) {
+    
     this.gs.getData().subscribe( res => {
       this.itemDetails = res['themes'].filter(function(el) {
         return el._id == arg;
       });
-      this.items = this.itemDetails[0].books;
+      if(this.itemDetails[0].books.length) {
+        this.items = this.itemDetails[0].books;
+      } else {
+        this.noDetails = true;
+      }
+      this.loading = false;
     });
   }
 }
